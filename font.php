b3252ea1e9d2bd4c42c7954d00d0937d0ccf684d
Mint2307 <98528824+Mint2307@users.noreply.github.com>
2022-03-11 11:11:52 +0700

    Create  font.php

---
+++ font.php
@@ -0,0 +1,23 @@
+<html>
+  <head>
+    <title>การกำหนดรูปแบบตัวอักษร</title>
+    <style>
+      @import url('https://fonts.googleapis.com/css2?family=Charm:wght@400;700&display=swap');
+      @import url('https://fonts.googleapis.com/css2?family=Pattaya&display=swap');
+      body {
+        font-size: 1.25rem;
+        text-align: center;
+      }
+      .charm {
+        font-family: 'Charm', cursive;
+      }
+      .pattaya {
+        font-family: 'Pattaya', sans-serif;
+      }
+    </style>
+  </head>
+  <body>
+    <h1 class="charm">ศรัญญา กิ่งกระโทก เลขที่ 11 ห้อง ม.4/12 เลขประจำตัว 56702</h1>
+    <h1 class="pattaya">ศรัญญา กิ่งกระโทก เลขที่ 11 ห้อง ม.4/12 เลขประจำตัว 56702</h1>
+  </body>
+</html>
